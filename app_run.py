ecc54dc0f6c5b044bca4c66847f10e64693dfb9c
WeiWeiCheng123 <60820468+WeiWeiCheng123@users.noreply.github.com>
2020-04-21 00:44:24 +0800

    Add files via upload

---
+++ app_run.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from flask import *
+import numpy
+import pandas as pd
+numpy.random.seed(10)
+import fileinput
+from flask_sqlalchemy import SQLAlchemy
+from dbModel import *
+import os
+import xlrd
+
+new_model = tf.keras.models.load_model('spec.h5')
+
+#app = Flask(__name__)
+@app.route('/')
+@app.route('/index')
+def index():
+
+    return render_template('index.html', **locals())
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+     if request.method == 'POST':
+             file = request.files['inputfile']
+             newfile= FileContents(name=file.filename, data=file.read())
+             db.session.add(newfile)
+             db.session.commit()
+     return redirect(url_for('index'))
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+     if request.method == 'POST':
+             file = request.files['inputfile']
+             test_df = pd.read_excel(file)
+             test_df=test_df.values
+             test_df = numpy.array(test_df, dtype=numpy.float32)
+             predictions = new_model.predict(test_df)
+             if (predictions[0][0]>predictions[0][1])&(predictions[0][0]>predictions[0][2])&(predictions[0][0]>predictions[0][3]):
+                 results='1'
+             if (predictions[0][1]>predictions[0][0])&(predictions[0][1]>predictions[0][2])&(predictions[0][1]>predictions[0][3]):
+                 results='2'
+             if (predictions[0][2]>predictions[0][0])&(predictions[0][2]>predictions[0][1])&(predictions[0][2]>predictions[0][3]):
+                 results='3'
+             if (predictions[0][3]>predictions[0][0])&(predictions[0][3]>predictions[0][1])&(predictions[0][3]>predictions[0][2]):
+                 results='4'
+     return render_template('index.html',results=results)
+        
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
---
+++ dbModel.py
@@ -0,0 +1,32 @@
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+import os
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tvlnxprmwxzwan:74c73bfe3c416cc5d0eb529c60e6e8619758976ec0faf2f2e415a8dcde47b1ba@ec2-3-231-46-238.compute-1.amazonaws.com:5432/d1avoq1j5ic1nh'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+class FileContents(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    data = db.Column(db.LargeBinary)
+
+    def __init__(self
+                , name
+                , data
+                ):
+         self.name = name
+         self.data = 	data
+
+if __name__ == '__main__':
+    manager.run()
